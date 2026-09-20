d0909ad153efc487e0e7782504eabc8437de99c4
montibonelenizelbitio <montibonelenizelbitio@gmail.com>
2026-09-20 17:40:41 +0800

    First commit sa akong Expo project files

---
+++ data/studentsData.js
@@ -0,0 +1,39 @@
+// data/studentsData.js
+export const students = [
+  { 
+    id: '1', 
+    name: 'Liam Vance', 
+    course: 'Bachelor of Science in Education Major in Mathematics',
+    image: 'https://plus.unsplash.com/premium_photo-1682096252599-e8536cd97d2b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI2fHx8ZW58MHx8fHx8'
+  },
+  { 
+    id: '2', 
+    name: 'Marcus Brody', 
+    course: 'Bachelor of Science in Computer Science',
+    image: 'https://plus.unsplash.com/premium_photo-1723478495487-c436ae82f48f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D' 
+  },
+  { 
+    id: '3', 
+    name: 'Ethan Hunt', 
+    course: 'Bachelor of Science in Electrical Engineering',
+    image: 'https://plus.unsplash.com/premium_photo-1682091992663-2e4f4a5534ba?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym95JTIwc3R1ZGVudHxlbnwwfHwwfHx8MA%3D%3D'
+  },
+  { 
+    id: '4', 
+    name: 'Clara Oswald', 
+    course: 'Bachelor of Science in Computer Technology',
+    image: 'https://images.unsplash.com/photo-1544717305-2782549b5136?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Z2lybCUyMHN0dWRlbnR8ZW58MHx8MHx8fDA%3D'
+  },
+  { 
+    id: '5', 
+    name: 'Priya Sharma', 
+    course: 'Bachelor of Science in Nursing',
+    image: 'https://media.istockphoto.com/id/966521024/photo/student-going-for-the-lecture.webp?a=1&b=1&s=612x612&w=0&k=20&c=ZKO9r2htdSU53z0oDdcVLg49olFzEEJXnV9HEhcDIEI='
+  },
+  { 
+    id: '6', 
+    name: 'Anjali Desai', 
+    course: 'Bachelor of Science in Psychology',
+    image: 'https://plus.unsplash.com/premium_photo-1682096200654-2f3297b0e9bd?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGdpcmwlMjBzdHVkZW50fGVufDB8fDB8fHww'
+  },
+];
